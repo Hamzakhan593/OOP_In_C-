5dc24c1a9f690bf76c28f4ef3c23e6970a481e02
muhammad hamza khan <mh9813951@gmail.com>
2026-02-05 11:52:55 +0500

    Add project files.

---
+++ OOP_In_C#/Program.cs
@@ -0,0 +1,55 @@
+﻿using OOP_In_C_;
+
+Student Jahangir = new Student();
+Jahangir.Name = "Jahangir";
+Jahangir.Age = -24;
+Jahangir.RollNo = "23bsit04";
+Jahangir.Batch = "2023";
+Jahangir.Course = "BSIT";
+Jahangir.Department = "IT";
+Jahangir.Address = "Karachi";
+Jahangir.Univerity = "SBBU";
+
+Student Hamza = new Student();
+Hamza.Name = "Hamza";
+Hamza.Age = 24;
+Hamza.RollNo = "23bsit02";
+Hamza.Batch = "2023";
+Hamza.Course = "BSIT";
+Hamza.Department = "IT";
+Hamza.Address = "Karachi";
+Hamza.Univerity = "SBBU";
+
+Student Shahzeb = new Student();
+Shahzeb.Name = "Shahzeb";
+Shahzeb.Age = 17;
+Shahzeb.RollNo = "23bsit37";
+Shahzeb.Batch = "2023";
+Shahzeb.Course = "BSIT";
+Shahzeb.Department = "BBA";
+Shahzeb.Address = "Karachi";
+Shahzeb.Univerity = "SBBU";
+
+// This will show the student information of Jahangir and that the isTrue method
+Jahangir.studentInfoPrint();
+Console.WriteLine(Jahangir.isTrue());
+Console.WriteLine();
+
+// This will show the student information of Jahangir and that the isTrue method
+Hamza.studentInfoPrint();
+Console.WriteLine(Hamza.isTrue());
+Console.WriteLine();
+
+// This will show that the isTrue method returns true for Jahangir and Hamza
+// because their age is greater than 18, while it returns false for Shahzeb
+// because his age is less than 18.
+Shahzeb.studentInfoPrint();
+Console.WriteLine(Shahzeb.isTrue());
+Console.WriteLine();
+
+// This will show that when we assign one object to another, they both point
+// to the same memory location. So, modifying one will affect the other.
+Student s1 = Jahangir;
+s1.Name = "Modified Name";
+Console.WriteLine(Jahangir.Name);
+
---
+++ OOP_In_C#/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace OOP_In_C_
+{
+    public class Student
+    {
+        //public string name;
+        //public int age;
+        //public string RollNo;
+        //public string Batch;
+        //public string Course;
+        //public string department;
+        //public string Address;
+        //public string Univerity;
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string RollNo { get; set; }
+        public string Batch { get; set; }
+        public string Course { get; set; }  
+        public string Department { get; set; }
+        public string Address { get; set; }
+        public string Univerity { get; set; }
+
+
+        public void studentInfoPrint()
+        {
+            Console.WriteLine($"Student Information of:{Name}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"RollNo: {RollNo}");
+            Console.WriteLine($"Batch: {Batch}");
+            Console.WriteLine($"Course: {Course}");
+            Console.WriteLine($"department: {Department}");
+            Console.WriteLine($"Address: {Address}");
+            Console.WriteLine($"Univerity: {Univerity}");
+        }
+
+        public bool isTrue()
+        {
+            return true? Age > 18 : false;
+        }
+
+    }
+}
